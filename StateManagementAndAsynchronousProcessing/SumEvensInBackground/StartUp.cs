cd1b66c3db146e07b3a0eb76b9e3d828f94b9ebe
nikolaiandrov <nikolaiandrov@abv.bg>
2023-11-06 16:51:50 +0200

    one more task done

---
+++ StateManagementAndAsynchronousProcessing/SumEvensInBackground/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace SumEvensInBackground
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            long sum = 0;
+
+            Task task = Task.Run(() =>
+            {
+                for (long i = 1; i <= 1000000000; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        sum += i;
+                    }
+                }
+            });
+
+            string command;
+
+            while ((command = Console.ReadLine()!) != "exit")
+            {
+                if (command == "show")
+                {
+                    Console.WriteLine(sum);
+                }
+            }
+        }
+    }
+}
