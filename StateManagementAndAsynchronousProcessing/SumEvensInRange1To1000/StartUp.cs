a4b824b5014d901d59622e3f92fc329ab48e4ef1
nikolaiandrov <nikolaiandrov@abv.bg>
2023-11-06 15:52:14 +0200

    first steps into multithreading

---
+++ StateManagementAndAsynchronousProcessing/SumEvensInRange1To1000/StartUp.cs
@@ -0,0 +1,38 @@
+﻿namespace SumEvensInRange1To1000
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string command;
+
+            while ((command = Console.ReadLine()!) != "end")
+            {
+                if (command == "show")
+                {
+                    long result = SumAsync();
+                    Console.WriteLine(result);
+                }
+            }
+        }
+
+        private static long SumAsync()
+        {
+            return Task.Run(() =>
+            {
+                long sum = 0;
+
+                for (int i = 1; i < 10000; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        sum += i;
+                    }
+                }
+
+                return sum;
+
+            }).Result;
+        }
+    }
+}
