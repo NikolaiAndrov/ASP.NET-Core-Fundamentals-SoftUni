8c53c3ad5d8d41a32da489fa01fc5624c03dcbe0
nikolaiandrov <nikolaiandrov@abv.bg>
2023-11-06 15:33:05 +0200

    First step into multithreading

---
+++ StateManagementAndAsynchronousProcessing/EvenNumbersThread/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace EvenNumbersThread
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int startNumber = int.Parse(Console.ReadLine()!);
+            int endNumber = int.Parse(Console.ReadLine()!);
+
+            Thread evenNumbers = new Thread(() => PrintEveneNumbers(startNumber, endNumber));
+
+            evenNumbers.Start();
+            evenNumbers.Join();
+
+            Console.WriteLine("Thread finished work");
+        }
+
+        static void PrintEveneNumbers(int startNumber, int endNumber)
+        {
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
