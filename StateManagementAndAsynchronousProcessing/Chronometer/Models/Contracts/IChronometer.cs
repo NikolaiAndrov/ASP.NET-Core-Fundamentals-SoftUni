39cd8cc96589a3266e4bbec45002f9d6dc86ec2d
nikolaiandrov <nikolaiandrov@abv.bg>
2023-11-06 17:45:58 +0200

    Implementing simple asynchronous chronometer

---
+++ StateManagementAndAsynchronousProcessing/Chronometer/Models/Contracts/IChronometer.cs
@@ -0,0 +1,17 @@
+﻿namespace Chronometer.Models.Contracts
+{
+    public interface IChronometer
+    {
+        string GetTime { get; }
+
+        List<string> Laps { get; }
+
+        void Start();
+
+        void Stop();
+
+        void Reset();
+
+        string Lap();
+    }
+}
---
+++ StateManagementAndAsynchronousProcessing/Chronometer/StartUp.cs
@@ -0,0 +1,58 @@
+﻿namespace Chronometer
+{
+    using Models;
+    using Chronometer.Models.Contracts;
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            IChronometer chronometer = new Chronometer();
+
+            string command;
+
+            while ((command = Console.ReadLine()!) != "exit")
+            {
+                if (command == "start")
+                {
+                    Task.Run(() =>
+                    {
+                        chronometer.Start();
+                    });
+                }
+                else if (command == "stop")
+                {
+                    chronometer.Stop();
+                }
+                else if (command == "lap")
+                {
+                    Console.WriteLine(chronometer.Lap());
+                }
+                else if(command == "laps")
+                {
+                    if (chronometer.Laps.Count == 0)
+                    {
+                        Console.WriteLine("Laps: No laps");
+                        continue;
+                    }
+
+                    Console.WriteLine("Laps:");
+
+                    for (int i = 0; i < chronometer.Laps.Count; i++)
+                    {
+                        Console.WriteLine($"{i}. {chronometer.Laps[i]}");
+                    }
+                }
+                else if (command == "reset")
+                {
+                    chronometer.Reset();
+                }
+                else if (command == "time")
+                {
+                    Console.WriteLine(chronometer.GetTime);
+                }
+            }
+
+            chronometer.Stop();
+        }
+    }
+}
