104990258a388dc7f7a6fca132c167697b91cd0e
nikolaiandrov <nikolaiandrov@abv.bg>
2023-11-14 23:12:16 +0200

    Creating simple chat

---
+++ ASP.NETCoreIntroduction/ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,42 @@
+﻿namespace ChatApp.Controllers
+{
+	using ChatApp.Models.Message;
+	using Microsoft.AspNetCore.Mvc;
+
+	public class ChatController : Controller
+	{
+		private static ICollection<KeyValuePair<string, string>> messages =
+			new List<KeyValuePair<string, string>>();
+
+		public IActionResult Show()
+		{
+			if (messages.Count < 1)
+			{
+				return View(new ChatViewModel());
+			}
+
+			var chatModel = new ChatViewModel()
+			{
+				Messages = messages
+				.Select(m => new MessageViewModel()
+				{
+					Sender = m.Key,
+					MessageText = m.Value
+				})
+				.ToList()
+			};
+
+			return View(chatModel);
+		}
+
+		[HttpPost]
+		public IActionResult Send(ChatViewModel chat)
+		{
+			var newMessages = chat.CurrentMessage;
+
+			messages.Add(new KeyValuePair<string, string>(newMessages.Sender, newMessages.MessageText));
+
+			return RedirectToAction("Show");
+		}
+	}
+}
---
+++ ASP.NETCoreIntroduction/ChatApp/Models/Message/MessageViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ChatApp.Models.Message
+{
+	using System.ComponentModel.DataAnnotations;
+
+	public class MessageViewModel
+	{
+		[Required]
+		public string Sender { get; set; } = null!;
+
+		[Required]
+		public string MessageText { get; set; } = null!;
+	}
+}
